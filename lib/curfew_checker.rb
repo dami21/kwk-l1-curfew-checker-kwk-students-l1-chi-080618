3330189c76a9d2974b2194110c13ec796b4bcf55
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-07 19:19:52 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -15,21 +15,25 @@ end
 def complex_curfew_checker(time)
   if time == 11
     "Time to apparate!"
-  else 
+  elsif time > 11
      "You're in trouble! Better get home quick!"
-  elsif
+  else
     "Keep having fun!"
   end
 end
 
 def deluxe_curfew_checker(time)
-  if time >= 11
-    "You're in trouble! Better get home quick!"
-  
+  if time == 11
+    "Time to apparate!"
+  elsif time > 11
+     "You're in trouble! Better get home quick!"
+  else
+    "you have #{11-time} hours left to keep having fun!"
+  end
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  if time == 11
 end
 
 num_of_apples = 6
